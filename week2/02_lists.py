e9ff42be37c460cd4212bb8862c29c4f72767844
Angelo <asanon@udel.edu>
2026-02-09 11:01:42 -0500

    Lists

---
+++ week2/02_lists.py
@@ -0,0 +1,7 @@
+titles = ["Homework1: Intro to database", "Homework2: Normalization", "Case Study: Requirements"]
+
+titles.append("Project Milestone: ER Diagram Design")
+
+print("titles", titles)
+print(f"First Assignment Titles: {titles[0]}")
+print("Total Number of Assignments: ", len{titles})
